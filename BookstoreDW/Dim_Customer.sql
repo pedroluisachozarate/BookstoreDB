64c1b1e36aad2c81563f379c40c652e43e384cb1
pedroluisachozarate <achozarate@gmail.com>
2025-12-02 17:31:01 -0400

    Reorganización del proyecto: agrego carpetas BookstoreDW y BookstoreDB_OLTP con scripts DW y ETL

--- BookstoreDW/Dim_Customer.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE DimCustomer (
-    CustomerKey INT IDENTITY(1,1) PRIMARY KEY,
-    CustomerID INT,
-    FirstName VARCHAR(100),
-    LastName VARCHAR(100),
-    Email VARCHAR(200),
-    Country VARCHAR(100)
-);
-GO
--- BookstoreDW/Dim_ShippingMethod.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE DimShippingMethod (
-    ShippingMethodKey INT IDENTITY(1,1) PRIMARY KEY,
-    MethodID INT,
-    MethodName VARCHAR(100),
-    Cost DECIMAL(10,2)
-);
-GO
